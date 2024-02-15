ae728a7ddb441c0883217b698d12cd8d5c099dc3
Haybhie <Haybhiee@gmail.com>
2024-02-15 04:32:34 +0000

    add

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,6 @@
--- Creates the database hbtn_0c_0.
-CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
+-- script that creates the MySQL server user user_0d_1.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+    ON *.*
+    TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
